004c3cf9d1006ddf48fa67d9e4cbd9a90f9f2001
Jiseung Han <49984198+chldkato@users.noreply.github.com>
2020-05-03 16:01:00 +0900

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -61,7 +61,7 @@ def write_metadata(metadata, out_dir):
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--base_dir', default=os.path.expanduser('~/tacotron'))
+  parser.add_argument('--base_dir', default='./')
   parser.add_argument('--output', default='training')
   parser.add_argument('--num_workers', type=int, default=cpu_count())
   args = parser.parse_args()
